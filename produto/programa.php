750c99ccf62d3d02e6df90a84f9af9ba849f2fc3
giseleyasmin <gisele_gege@hotmail.com>
2013-12-18 12:13:25 -0200

    Create programa.php

---
+++ produto/programa.php
@@ -0,0 +1,21 @@
+<?php
+
+$codigo = 0;
+
+$codigo = $_POST["codigo"];
+	
+	if ($codigo == 1) {
+		echo "caneta => 1.20";
+	}
+	elseif ($codigo == 2) {
+		echo "lapis => 0.80";
+	}
+	elseif (codigo == 3) {
+		echo "caderno => 4.50";
+	else
+	}
+	    echo "produto nao castrado";
+ 
+	}
+	
+	?>
