f4677932b3df006d8e47e3140cf13dccd3408626
giseleyasmin <gisele_gege@hotmail.com>
2014-01-10 12:25:43 -0200

    Create campos_pessoa.php

---
+++ avaliacao3/campos_pessoa.php
@@ -0,0 +1,16 @@
+  <label>Nome do Produto: <br/></label><input type="text" name="nomeProduto" />
+            <br/>
+            <input type="checkbox" value="sim" name="produtoNovo" />
+            <label>Produto Novo: <br/></label>
+            <input type="radio" name="cor" value="azul" checked="checked" /><label>Azul</label>
+            <input type="radio" name="cor" value="verde" /><label>Verde</label>
+            <input type="radio" name="cor" value="vermelho" /><label>Vermelho</label>
+            <br/>
+            <label>Prazo de Entrega: <br/></label>
+            <select name="prazo">
+                <option>5 Dias</option>
+                <option>10 Dias</option>
+       		<option>15 Dias</option>
+                </select>
+            <br/>
+            <label>Detalhe do Produto: </label><br/><textarea name="detalhe" rows="2" cols="20"/></textarea>
