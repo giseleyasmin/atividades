6c0d20e63645fae44e64e395475bae3ee22f659d
giseleyasmin <gisele_gege@hotmail.com>
2014-01-13 09:55:46 -0200

    Create editar.php

---
+++ cadastrolivros/editar.php
@@ -0,0 +1,25 @@
+<?php
+    require_once("menu.php");
+    
+    session_start();
+    
+    $id = $_REQUEST["id"];
+    $nomeLivro = $_REQUEST["nomeLivro"];
+    $autor = $_REQUEST["autor"];
+    $isbn = $_REQUEST["isbn"];
+    $genero = $_REQUEST["genero"];
+    $formato = $_REQUEST["formato"];
+    $sinopse = $_REQUEST["sinopse"];
+    
+    if(!isset($_SESSION["cadastroLivros"])) {
+        echo "Não existem cadastros para editar";
+    }
+    else {
+        $cadastroLivros =& $_SESSION["$cadastroLivros"];
+        $livro = array("nomeLivro" => $nomeLivro, "autor" => $autor, "isbn" => $isbn, "genero" => $genero, "formato" => $formato, "sinopse" => $sinopse);
+        $cadastrolivros[$id] = $livro;
+        }
+    
+    echo "Edição efetuado com sucesso!";
+?>
+    
