4c864f015da832bc89b83a8b7ef7f3d262d0db26
giseleyasmin <gisele_gege@hotmail.com>
2014-01-13 09:57:28 -0200

    Create menu.php

---
+++ cadastrolivros/menu.php
@@ -0,0 +1,9 @@
+<a href="index.php">Listar</a>
+&nbsp;&nbsp;
+<a href="cadastrar_form.php">Cadastrar</a>
+&nbsp;&nbsp;
+<a href="remover_form.php">Remover</a>
+&nbsp;&nbsp;
+<a href="editar_form.php">Editar</a>
+&nbsp;&nbsp;
+<br/><br/>
