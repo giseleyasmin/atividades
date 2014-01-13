02171dc6bdb52752c10446a1add60f33ee908ab0
giseleyasmin <gisele_gege@hotmail.com>
2014-01-13 09:55:20 -0200

    Create campos_pessoa.php

---
+++ cadastrolivros/campos_pessoa.php
@@ -0,0 +1,47 @@
+        <label>Nome do Livro: <br/></label><input type="text" name="nomeLivro" />
+            <br/>  
+        <label>Nome do Autor: <br/></label><input type="text" name="autor" />
+            <br/>
+        <label>ISBN: <br/></label><input type="text" name="isbn" />
+            <br/>
+            <input type="checkbox" value="sim" name="novo" />
+            <label> Livro Novo: </label>
+            <br/>
+            <input type="radio" name="genero" value="Romance" checked="checked" /><label>Romance</label>
+            <br/>
+            <input type="radio" name="genero" value="Musical" /><label>Musical</label>
+            <br/>
+            <input type="radio" name="genero" value="Ficção cientifíca" /><label>Ficção cientifíca</label>
+            <br/>
+            <input type="radio" name="genero" value="Ficção Policial" /><label>Ficção Policial</label>
+            <br/>
+            <input type="radio" name="genero" value="Aventura" /><label>Aventura</label>
+            <br/>
+            <input type="radio" name="genero" value="Comèdia" /><label>Comèdia</label>
+            <br/>
+            <input type="radio" name="genero" value="Drama" /><label>Drama</label>
+            <br/>
+            <input type="radio" name="genero" value="Guerra" /><label>Guerra</label>
+            <br/>
+            <input type="radio" name="genero" value="Literatura Nacional " /><label>Literatura Nacional</label>
+            <br/>
+            <input type="radio" name="genero" value="Literatura Estrangeira" /><label>Literatura Estrangeira</label>
+            <br/>
+            <input type="radio" name="genero" value="Crônicas" /><label>Crônicas</label>
+            <br/>
+            <br/>
+            <label>Formato do Livro: <br/></label>
+            <select name="formato">
+                <option>E-book</option>
+                <option>Capa Dura</option>
+                <option>Edição de Bolso</option>
+                <option>Revista</option>
+                <option>Brochura</option>
+                <option>Grampo</option>
+                <option>Digital</option>
+            </select>
+                <br/>
+                <label>Sinopse do Livro: <br/></label>
+                <textarea name="sinopse" rows="5" cols="20"></textarea> 
+                
+                
