70636edde464459974818304ed1e847f02a38828
giseleyasmin <gisele_gege@hotmail.com>
2014-01-14 12:48:13 -0200

    Create editar.php

---
+++ cadastropessoa/editar.php
@@ -0,0 +1,34 @@
+<?php
+    require_once("menu.php");
+    
+    session_start();
+    
+    $id = $_REQUEST["id"];
+    $novoNome = $_REQUEST["nome"];
+    $sexo = $_REQUEST["sexo"];
+    $idade = $_REQUEST["idade"];
+    $nomeMae = $_REQUEST["nomeMae"];
+    $nomePai = $_REQUEST["nomePai"];
+    $estado = $_REQUEST["estado"];
+    
+    $aceito = false;
+    if (isset($_REQUEST["aceito"])) {
+        $aceito = true;
+    }
+    if(!isset($_SESSION["cadastros"])) {
+        echo "Não existem cadastros para editar";
+    }
+        else{
+            $cadastros =& $_SESSION["cadastros"];
+            $pessoa = array("nome" => $novoNome,
+                            "sexo" => $sexo,
+                            "idade" => $idade,
+                            "nomeMae" => $nomeMae,
+                            "nomePai" => $nomePai,
+                            "aceito" => $aceito,
+                            "estado" => $estado);
+            $cadastros[$id] = $pessoa;
+        }
+    
+    echo "Edição efetuado com sucesso!";
+?>
