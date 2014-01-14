ed9fd6e7c0e1b23c88ea8d6e7663a79ede382a1d
giseleyasmin <gisele_gege@hotmail.com>
2014-01-14 12:47:42 -0200

    Create campos_pessoa.php

---
+++ cadastropessoa/campos_pessoa.php
@@ -0,0 +1,38 @@
+ <label>Nome: <br/></label><input type="text" name="nome" />
+              <br/><br/>
+            <label>Idade: <br/></label><input type="text" name="idade" />
+              <br/><br/>  
+                <input type="radio" name="sexo" value="Masculino" checked="checked" /><label>Masculino</label>
+                <input type="radio" name="sexo" value="Feminino" /><label>Feminino</label>
+              <br/><br/>
+              <label>Nome da Mãe:  <br/></label><input type="text" name="nomeMae" />
+              <br/><br/>
+              <label>Nome do Pai:  <br/></label><input type="text" name="nomePai" />
+              <br/><br/>
+              <input type="checkbox" value="sim" name="aceito" />
+              <label> Aceito o Contrato</label>
+              <br/>
+              <select name="estado">
+                <option value = "-1">Selecione</option>
+                <option>Bahia</option>
+                <option>São Paulo</option>
+                <option>Rio de Janeiro</option>
+                <option>Alagoas</option>
+                <option>Mato Grosso</option>
+                <option>Distrito Federal</option>
+                <option>Pernambuco</option>
+                <option>Minas Gerais</option>
+                <option>Santa Catarina</option>
+                <option>Rondônia</option>
+                <option>Sergipe</option>
+                <option>Tocantins</option>
+                <option>Piauí</option>
+                <option>Paraíba</option>
+                <option>Rio Grande do Norte</option>
+                <option>Rio Grande do Sul</option>
+                <option>Maranhão</option>
+                <option>Amazonas</option>
+                <option>Goiás</option>
+                <option>Acre</option>
+              </select>
+              
