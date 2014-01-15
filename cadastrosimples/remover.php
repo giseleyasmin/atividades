f22225c7688f96298843126ca058556532a3561c
giseleyasmin <gisele_gege@hotmail.com>
2014-01-15 12:44:22 -0200

    Create remover.php

---
+++ cadastrosimples/remover.php
@@ -0,0 +1,16 @@
+<?php 
+    require_once("menu.php");
+
+    session_start();
+    
+    $id = $_REQUEST["id"];
+    
+    if(!isset($_SESSION["cadastros"])){
+       echo "Não existem cadastros para remover";
+    }
+    else{
+        $cadastros =& $_SESSION["cadastros"];
+        $cadastros[$id] = null;
+    }
+    echo "Remoção efetuada com sucesso!";
+?>
