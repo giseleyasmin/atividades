29bf21165f2e07ec6bd1a805a59ea04b6a4fcf5d
giseleyasmin <gisele_gege@hotmail.com>
2014-01-15 12:35:38 -0200

    Update cadastrar.php

--- cadastropessoa/cadastrar.php
+++ cadastropessoa/cadastrar.php
@@ -3,22 +3,27 @@
     
     session_start();
     
+    if(!isset($_SESSION["cadastros"])) {
+        $_SESSION["cadastros"] = array();
+    } 
+    
     $nome = $_REQUEST["nome"];
     $sexo = $_REQUEST["sexo"];
     $idade = $_REQUEST["idade"];
     $nomeMae = $_REQUEST["nomeMae"];
     $nomePai = $_REQUEST["nomePai"];
     $estado = $_REQUEST["estado"];
+    $texto = $_REQUEST["texto"];
     
     $aceito = false;
     if (isset($_REQUEST["aceito"])) {
         $aceito = true;
     }
     
-    if(!isset($_SESSION["cadastros"])) {
-        $_SESSION["cadastros"] = array();
-    }
+
     $camposValidados = true;
+    
+    $nome = trim($nome);
     
     if(empty($nome)){
         echo "Por favor, Preencha o campo nome <br/>";
@@ -32,10 +37,12 @@
         echo "Por favor, Preencha o campo idade <br/>";
         $camposValidados = false;
     }
+    $nomeMae = trim($nomeMae);
      if(empty($nomeMae)){
         echo "Por favor, Preencha o campo nomeMae <br/>";
         $camposValidados = false;
     }
+    $nomePai = trim($nomePai);
     if(empty($nomePai)){
         echo "Por favor, Preencha o campo nomePai <br/>";
         $camposValidados = false;
@@ -44,18 +51,25 @@
         echo "Por favor, Preencha o campo estado <br/>";
         $camposValidados = false;
     }
+    $texto = trim($texto);
+    if(empty($texto)){
+        echo "Por favor, Preencha o campo texto <br/>";
+        $camposValidados = false;
+    }
+    
     if($camposValidados){
-    $pessoa = array("nome" => $nome,
-                    "sexo" => $sexo,
-                    "idade" => $idade,
-                    "nomeMae" => $nomeMae,
-                    "nomePai" => $nomePai,
-                    "estado" => $estado,
-                    "aceito" => $aceito);
-    
-    array_push($_SESSION["cadastros"], $pessoa);
-    
-    echo "Cadastro efetuado com sucesso!";
+        $pessoa = array("nome" => $nome,
+                        "sexo" => $sexo,
+                        "idade" => $idade,
+                        "nomeMae" => $nomeMae,
+                        "nomePai" => $nomePai,
+                        "estado" => $estado,
+                        "aceito" => $aceito,
+                        "texto" => $texto);
+        
+        array_push($_SESSION["cadastros"], $pessoa);
+        
+        echo "Cadastro efetuado com sucesso!";
     }
     else{
         echo "<br/>";
