52455b837e5f6996547c859d2f3fc12e4770914c
giseleyasmin <gisele_gege@hotmail.com>
2014-01-15 12:36:36 -0200

    Update campos_pessoa.php

--- cadastropessoa/campos_pessoa.php
+++ cadastropessoa/campos_pessoa.php
@@ -1,19 +1,19 @@
  <label>Nome: <br/></label><input type="text" name="nome" />
-              <br/><br/>
+              <br/>
             <label>Idade: <br/></label><input type="text" name="idade" />
-              <br/><br/>  
-                <input type="radio" name="sexo" value="Masculino" checked="checked" /><label>Masculino</label>
+              <br/> 
+                <label>Sexo:</label><input type="radio" name="sexo" value="Masculino" checked="checked" /><label>Masculino</label>
                 <input type="radio" name="sexo" value="Feminino" /><label>Feminino</label>
-              <br/><br/>
+              <br/>
               <label>Nome da Mãe:  <br/></label><input type="text" name="nomeMae" />
-              <br/><br/>
+              <br/>
               <label>Nome do Pai:  <br/></label><input type="text" name="nomePai" />
-              <br/><br/>
+              <br/>
               <input type="checkbox" value="sim" name="aceito" />
               <label> Aceito o Contrato</label>
               <br/>
               <select name="estado">
-                <option value = "-1">Selecione</option>
+                <option value = "-1">----->Selecione<-----</option>
                 <option>Bahia</option>
                 <option>São Paulo</option>
                 <option>Rio de Janeiro</option>
@@ -35,4 +35,11 @@
                 <option>Goiás</option>
                 <option>Acre</option>
               </select>
-              
+               <br/>
+               <label>Resenha:</label>
+               <br/>
+              <textarea name="texto" rows="3" cols="16"></textarea> 
+                
+                
+               
+          
